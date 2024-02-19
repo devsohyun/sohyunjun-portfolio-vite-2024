c1601cc03ffd7ff8dfe1595444a3cce5bc368958
devsohyun <iamsohyunjun@gmail.com>
2024-02-19 18:08:25 +0900

    Add model and set MeshTransmissionMaterial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import * as THREE from 'three'
-import { Canvas, useLoader } from '@react-three/fiber'
-import { Environment, Lightformer, OrbitControls } from '@react-three/drei'
-import { EffectComposer, Bloom, LUT, BrightnessContrast, HueSaturation, ToneMapping } from '@react-three/postprocessing'
+import * as THREE from "three"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { Environment, Lightformer, OrbitControls } from "@react-three/drei"
+import {
+  EffectComposer,
+  Bloom,
+  LUT,
+  BrightnessContrast,
+  HueSaturation,
+  ToneMapping,
+} from "@react-three/postprocessing"
 import { LUTCubeLoader, ToneMappingMode } from "postprocessing"
-import Flower from "./componenets/Flower"
+import Model from "./componenets/Model"
 import "./styles/main.scss"
 
 const App = () => {
@@ -11,33 +18,30 @@ const App = () => {
   return (
     <Canvas
       gl={{ antialias: false }}
-      camera={{ position: [0, 2.5, 5], fov: 35 }}
+      camera={{ position: [15, -8, 15], fov: 15 }}
       onCreated={(state) => {
         state.gl.toneMapping = THREE.NoToneMapping
       }}
     >
-      <color attach='background' args={["#151520"]} />
+      <color attach='background' args={["#151515"]} />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Flower position={[0, -0.25, 0]} />
+      <Model position={[0, 0, 0]} />
       <OrbitControls />
-      <Environment
-        files='./images/peppermint_powerplant_1k.hdr'
-        resolution={512}
-      >
+      <Environment files='./images/peppermint_powerplant_1k.hdr' resolution={512}>
         <group rotation={[0, 0, 1]}>
           <Lightformer
             form='circle'
-            intensity={10}
-            position={[0, 10, -10]}
+            intensity={5}
+            position={[0, 5, -10]}
             scale={20}
             onUpdate={(self) => self.lookAt(0, 0, 0)}
           />
           <Lightformer
             intensity={0.1}
             onUpdate={(self) => self.lookAt(0, 0, 0)}
-            position={[-5, 1, -1]}
+            position={[-5, 20, -1]}
             rotation-y={Math.PI / 2}
             scale={[50, 10, 1]}
           />
@@ -54,6 +58,12 @@ const App = () => {
             onUpdate={(self) => self.lookAt(0, 0, 0)}
             position={[0, 1, 0]}
             scale={[10, 100, 1]}
+          />
+          <Lightformer
+            intensity={10}
+            position={[-20, -8, 15]}
+            scale={10}
+            onUpdate={(self) => self.lookAt(0, 0, 0)}
           />
         </group>
       </Environment>
---
+++ src/componenets/Model.jsx
@@ -0,0 +1,53 @@
+import { MeshTransmissionMaterial, useGLTF } from "@react-three/drei"
+
+const Flower = (props) => {
+  const { nodes } = useGLTF("/models/torus-through-planes.glb")
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Plane.geometry}
+        position={[0, -0.007, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
+        scale={[1.906, 2.541, 1.906]}
+      >
+        <MeshTransmissionMaterial
+          backside
+          samples={16}
+          resolution={256}
+          transmission={1}
+          roughness={0.6}
+          thickness={0.08}
+          backsideThickness={0.05} //1
+          ior={1.0}
+          chromaticAberration={3}
+          anisotropy={1}
+          distortion={0}
+          distortionScale={0.5}
+          temporalDistortion={0}
+          attenuationDistance={0.5}
+          anisotropicBlur={0.1}
+          envMapIntensity={0.3}
+          color={"#b6b6b6"}
+        />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Torus.geometry}
+        material={nodes.Torus.material}
+        rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+      >
+        {/* <MeshTransmissionMaterial
+            samples={6}
+            resolution={256}
+            thickness={-1}
+            anisotropy={0.25}
+          /> */}
+      </mesh>
+    </group>
+  )
+}
+
+export default Flower
