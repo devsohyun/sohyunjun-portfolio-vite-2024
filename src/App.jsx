f137d79b5a94f90febb272c978a3cd3c68693d8c
devsohyun <iamsohyunjun@gmail.com>
2024-02-27 11:13:38 +0900

    Reorganize files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import * as THREE from "three"
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls, Scroll, ScrollControls } from "@react-three/drei"
 import "./styles/main.scss"
-import { Experience } from "./componenets/Experience"
-import { Interface } from "./componenets/sections/Interface"
-import { useEffect, useState } from "react"
-import { ScrollManager } from "./componenets/ScrollManager"
-import { Nav } from "./componenets/Nav"
+import { Suspense, useState } from "react"
+import { Nav } from "./componenets/common/Nav"
+import { Experience } from "./componenets/experience/Experience"
 
 const App = () => {
   const [section, setSection] = useState(0)
@@ -14,28 +9,14 @@ const App = () => {
 
   return (
     <>
-      <Canvas
-        gl={{ antialias: false }}
-        camera={{ position: [15, -8, 15], fov: 15 }}
-        onCreated={(state) => {
-          state.gl.toneMapping = THREE.NoToneMapping
-        }}
-      >
-        <color attach='background' args={["#2c2c2c"]} />
-        {/* <OrbitControls /> */}
-        <ScrollControls pages={6.5} damping={0.1}>
-          <ScrollManager section={section} onSectionChange={setSection} />
-          <Experience menuOpened={menuOpened} />
-          <Scroll html>
-            <Interface />
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
-      <Nav
-        onSectionChange={setSection}
-        menuOpened={menuOpened}
-        setMenuOpened={setMenuOpened}
-      />
+      <Suspense fallback={null}>
+        <Experience section={section} setSection={setSection} />
+        <Nav
+          onSectionChange={setSection}
+          menuOpened={menuOpened}
+          setMenuOpened={setMenuOpened}
+        />
+      </Suspense>
     </>
   )
 }
